934f2a6be2347e5eabb54d5e0e9bd1cb72eeb96f
hako 生活 <44082431+clrfnd@users.noreply.github.com>
2020-12-11 07:35:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,15 +4,4 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-CHANNEL_ID = 786716037718474752
-
-@bot.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    if  == '/aiueo':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('メッセージ')
-
 bot.run(token)
