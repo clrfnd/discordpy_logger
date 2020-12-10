46b2a214ff8d7d28cecaeadcc8a1cb5b4a069166
hako 生活 <44082431+clrfnd@users.noreply.github.com>
2020-12-11 07:32:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,17 +1,10 @@
 from discord.ext import commands
 import os
 import traceback
-import logging
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 global CHANNEL_ID = 786716037718474752
-
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
 
 @bot.event
 async def on_message(message):
