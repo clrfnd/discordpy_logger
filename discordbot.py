083ed446057b6f940d6033d8f2a6282334e29eac
hako 生活 <44082431+clrfnd@users.noreply.github.com>
2020-12-11 07:40:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,12 +3,13 @@ import os
 import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
+CHANNEL_ID = 786716037718474752
 @bot.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    await message.channel.send('メッセージ')
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('メッセージ')
         
 bot.run(token)
