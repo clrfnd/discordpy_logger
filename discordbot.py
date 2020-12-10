44f4783093f46e2f25617595f00ebc0833aad624
hako 生活 <44082431+clrfnd@users.noreply.github.com>
2020-12-11 06:55:09 +0900

    レスポンスのテスト

--- discordbot.py
+++ discordbot.py
@@ -6,16 +6,13 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    if message.content == '/aiueo':
+        await message.channel.send('あいうえお')
 
 
 bot.run(token)
