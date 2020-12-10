dbe5a15dfff97b22501871038ca892f2c81842ba
hako 生活 <44082431+clrfnd@users.noreply.github.com>
2020-12-11 07:23:24 +0900

    エラーハンドラのテスト

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,12 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 global CHANNEL_ID = 786716037718474752
-
+@bot.event
+async def on_error(event, *args, **kwargs):
+       message = args[0] #Gets the message object
+       logging.warning(traceback.format_exc()) #logs the error
+       await bot.send_message(message.channel, "You caused an error!") #send the message to the channel
+        
 @bot.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
