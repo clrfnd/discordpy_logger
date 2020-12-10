b7ede8310ff77ed8ba0a3b3808ae977008f44801
hako 生活 <44082431+clrfnd@users.noreply.github.com>
2020-12-11 07:06:50 +0900

    通知のテスト

--- discordbot.py
+++ discordbot.py
@@ -5,14 +5,17 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+groval noticechannel
 
 @bot.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    if message.content == '/aiueo':
-        await message.channel.send('あいうえお')
-
+    if  == '/aiueo':
+        await '[' + message.channel + ']' + message.name + ':' + message.name
+        
+async def handle_channel(c)
+    noticechannel = c
 
 bot.run(token)
