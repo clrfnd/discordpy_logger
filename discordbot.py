3f53fa2a4396b97e94750d4f5e44f51068dc53f9
hako 生活 <44082431+clrfnd@users.noreply.github.com>
2020-12-11 17:53:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,8 @@ token = os.environ['DISCORD_BOT_TOKEN']
 LOG_CHANNEL_ID = 786696510439817226
 BROADCASTIN_CHANNEL_ID = 786757394754568233
 BROADCASTOUT_CHANNEL_ID = 786664256040730674
+INTRODUCE_CHANNEL_ID = 786661109247115285
+LOBBY_CHANNEL_ID = 786664600967315504
 @bot.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
@@ -14,12 +16,14 @@ async def on_message(message):
         return
     # メッセージ1
     #embed = discord.Embed(title="リンク集",description=f"メッセージのURLは [こちら]({message.jump_url}) をクリックしてください。")
-    
-    
     if message.channel.id == BROADCASTIN_CHANNEL_ID:
         if message.content[:3] == "YKZ":
             bchannel = bot.get_channel(BROADCASTOUT_CHANNEL_ID)
             await bchannel.send(message.content[3:])
+            return 
+        elif message.content[:3] == "TRI":
+            ichannel = bot.get_channel(INTRODUCE_CHANNEL_ID)
+            await ichannel.send(message.content[3:])
             return 
         else:
             await message.channel.send("放送に失敗しました。先頭にYKZを入れることで放送できます。")
