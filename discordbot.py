39955cfa76eee021cb9e7bbd6e5e29e4b21fd5cc
hako 生活 <44082431+clrfnd@users.noreply.github.com>
2020-12-11 10:02:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,8 @@ import traceback
 import os
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = 786696510439817226
+LOG_CHANNEL_ID = 786696510439817226
+BROADCAST_CHANNEL_ID = 786757394754568233
 @bot.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
@@ -13,13 +14,22 @@ async def on_message(message):
     # メッセージ1
     #embed = discord.Embed(title="リンク集",description=f"メッセージのURLは [こちら]({message.jump_url}) をクリックしてください。")
     
+    
+    if message.channel == BROADCAST_CHANNEL_ID:
+        if message.content[:3] == "YKZ":
+            bchannel = bot.get_channel(BROADCAST_CHANNEL_ID)
+            await bchannel.send(message.content[3:])
+            return 
+        else:
+            await bchannel.send("放送に失敗しました。先頭にYKZを入れることで放送できます。")
+            return 
     category = ''
     if message.channel.category_id:
         category = bot.get_channel(message.channel.category_id).name + ' '
     title = '[' + category + message.channel.name + '] ' + message.author.name
     # embed = discord.Embed(title=title,description=f"[メッセージへ]({message.jump_url})")    
     embed = discord.Embed(title=title,description= message.content + "\n" + f"[jump]({message.jump_url})")
-    channel = bot.get_channel(CHANNEL_ID)
+    channel = bot.get_channel(LOG_CHANNEL_ID)
     await channel.send(embed=embed)
         
 bot.run(token)
