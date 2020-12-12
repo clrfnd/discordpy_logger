98a77895d7cb6124f973dbcf3838e547c6cae0f4
hako 生活 <44082431+clrfnd@users.noreply.github.com>
2020-12-12 14:48:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -46,8 +46,8 @@ async def on_reaction_add(reaction, user):
     category = ''
     if reaction.message.channel.category_id:
         category = bot.get_channel(reaction.message.channel.category_id).name + ' '
-    title = '[' + category + reaction.message.channel.name + '] ' + user + 'さんがリアクションしました。'
-    embed = discord.Embed(title=title,description= reaction.emoji + "\n" + f"[jump]({reaction.message.jump_url})")
+    title = '[' + category + reaction.message.channel.name + '] ' + user.name + 'さんがリアクションしました。'
+    embed = discord.Embed(title=title,description= reaction.emoji.name + "\n" + f"[jump]({reaction.message.jump_url})")
     channel = bot.get_channel(LOG_CHANNEL_ID)
     await channel.send(embed=embed)
     
